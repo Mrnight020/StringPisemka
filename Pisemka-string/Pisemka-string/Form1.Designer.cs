051bee750b369bcbfdff728c229bbf3d18302f93
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2022-04-25 09:40:02 +0200

    Add files via upload

---
+++ Pisemka-string/Pisemka-string/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Pisemka_string
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Tomato;
+            this.button1.Location = new System.Drawing.Point(66, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spočítá nejčastější písmeno";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = " _a_ nam zabili    Ferdinanda.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(95, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zadej větu";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.Tomato;
+            this.button2.Location = new System.Drawing.Point(66, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 72);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Která malá písmena ještě chybějí ?\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(348, 403);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Pisemka-string/Pisemka-string/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pisemka_string
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string abeceda = "abcdefghijklmnopqrstuvwxyz ";
+
+            if(text != "")
+            {
+                while (text.Contains("  "))
+                {
+                    text = text.Replace("  ", " ");
+                }
+
+                char nejcastejsipismeno;
+
+                int pocet = 0;
+                int pocetpismeno = -1;
+                int pozice = 0;
+
+                for (int j = 0; j < 26; j++)
+                {
+                    foreach (char pismeno in text)
+                    {
+
+                        if (abeceda.Contains(pismeno))
+                        {
+                            if (pismeno == abeceda[j])
+                            {
+                                pocet++;
+                            }
+                        }
+                    }
+                    if (pocetpismeno < pocet)
+                    {
+                        pocetpismeno = pocet;
+                        pozice = j;
+                    }
+                    pocet = 0;
+                }
+
+                MessageBox.Show("nejpouzivanejsi pismeno je : " + abeceda[pozice]);
+            }
+            else
+            {
+                MessageBox.Show("Nic si nezadal!!");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string abeceda = "abcdefghijklmnopqrstuvwxyz ";
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in text)
+            {
+                if(abeceda.Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            string sbstring = sb.ToString();
+
+            StringBuilder neco = new StringBuilder();
+
+            foreach (char c in abeceda)
+            {
+                if (sbstring.Contains(c))
+                {
+                }
+                else
+                {
+                    neco.Append(c);
+                }
+            }
+
+            MessageBox.Show("Chybejici pismena = "+ neco);
+        }
+    }
+}
